3ab5b3d4bbcb42ed0608f4e1a3e9ea5f127a08fb
Dustin <lambright.dustin@gmail.com>
2015-06-19 17:00:57 -0700

    Connecting to Server now.

--- GameStats/GameStatsView.swift
+++ GameStats/GameStatsView.swift
@@ -52,8 +52,8 @@ class GameStatsView: UIViewController {
         picAwayTeam.image = awayTeamImage
         navBar.title = homeTeamURL + "     vs     " + awayTeamURL
         
-        let url: NSURL = NSURL(string: "localhost/" + homeTeamURL)!
-        //readHTML(url)
+        getHomeTeamData()
+        getAwayTeamData()
         
         
     }
@@ -61,16 +61,100 @@ class GameStatsView: UIViewController {
     func populateGameStatsView(newHomePicture: UIImage, newAwayPicture: UIImage) ->Void{
         homeTeamImage = newHomePicture
         awayTeamImage = newAwayPicture
+    }
+    
+    func populateHomeTeam(newScore: String, newFGMade: String, newFGAttempted: String, new3PMade: String, new3PAttempted: String, newFTMade: String, newFTAttempted: String, newDReb: String, newOReb: String, newTotalReb: String, newSteals: String, newBlocks: String, newAssists: String, newTSP: String, newWinProb: String)->Void{
         
-        
-        
-        
-        
+        lblHomeScore.text = newScore
+        lblHomeFG.text = newFGMade + "/" + newFGAttempted
+        lblHome3pt.text = new3PMade + "/" + new3PAttempted
+        lblHomeFT.text = newFTMade + "/" + newFTAttempted
+        lblHomeRebounds.text = newOReb + "/" + newDReb + "/" + newTotalReb
+        lblHomeSteals.text = newSteals
+        lblHomeBlocks.text = newBlocks
+        lblHomeAssists.text = newAssists
+        lblHomeTSP.text = newTSP
+        lblHomeWinProb.text = newWinProb
         
     }
     
-    func readHTML(givenURL: NSURL)->Void{
-        var e: NSError?
-        let string = NSString(contentsOfURL: givenURL, encoding: NSUTF8StringEncoding, error: &e)
+    func populateAwayTeam(newScore: String, newFGMade: String, newFGAttempted: String, new3PMade: String, new3PAttempted: String, newFTMade: String, newFTAttempted: String, newDReb: String, newOReb: String, newTotalReb: String, newSteals: String, newBlocks: String, newAssists: String, newTSP: String, newWinProb: String)->Void{
+        
+        lblAwayScore.text = newScore
+        lblAwayFG.text = newFGMade + "/" + newFGAttempted
+        lblAway3pt.text = new3PMade + "/" + new3PAttempted
+        lblAwayFT.text = newFTMade + "/" + newFTAttempted
+        lblAwayRebounds.text = newOReb + "/" + newDReb + "/" + newTotalReb
+        lblAwaySteals.text = newSteals
+        lblAwayBlocks.text = newBlocks
+        lblAwayAssists.text = newAssists
+        lblAwayTSP.text = newTSP
+        lblAwayWinProb.text = newWinProb
+        
     }
+    
+    func readHTML(givenURL: String)->NSString{
+        let url = NSURL(string: "http://" + givenURL)
+        var error: NSError?
+        let html = NSString(contentsOfURL: url!, encoding: NSUTF8StringEncoding, error: &error)
+        
+        if (error != nil) {
+            println("whoops, something went wrong")
+        } else {
+            println(html!)
+            return html!
+        }
+        return html!
+    }
+    
+    func getHomeTeamData()->Void{
+        let dataString : NSString! = readHTML("127.0.0.1/" + homeTeamURL)
+        
+        let lines = dataString.componentsSeparatedByString(", ")
+        var TSP :Float = lines[36].floatValue
+        TSP = Float(round(1000*TSP)/10)
+        
+        
+        populateHomeTeam(lines[0] as! String,
+            newFGMade: lines[2] as! String,
+            newFGAttempted: lines[3] as! String,
+            new3PMade: lines[5] as! String,
+            new3PAttempted: lines[6] as! String,
+            newFTMade: lines[8] as! String,
+            newFTAttempted: lines[9] as! String,
+            newDReb: lines[12] as! String,
+            newOReb: lines[11] as! String,
+            newTotalReb: lines[13] as! String,
+            newSteals: lines[15] as! String,
+            newBlocks: lines[16] as! String,
+            newAssists: lines[14] as! String,
+            newTSP: TSP.description + "%",
+            newWinProb: "TBD")
+    }
+    
+    func getAwayTeamData()->Void{
+        let dataString : NSString! = readHTML("127.0.0.1/" + awayTeamURL)
+        
+        let lines = dataString.componentsSeparatedByString(", ")
+        var TSP :Float = lines[36].floatValue
+        TSP = Float(round(1000*TSP)/10)
+        
+        
+        populateAwayTeam(lines[0] as! String,
+            newFGMade: lines[2] as! String,
+            newFGAttempted: lines[3] as! String,
+            new3PMade: lines[5] as! String,
+            new3PAttempted: lines[6] as! String,
+            newFTMade: lines[8] as! String,
+            newFTAttempted: lines[9] as! String,
+            newDReb: lines[12] as! String,
+            newOReb: lines[11] as! String,
+            newTotalReb: lines[13] as! String,
+            newSteals: lines[15] as! String,
+            newBlocks: lines[16] as! String,
+            newAssists: lines[14] as! String,
+            newTSP: TSP.description + "%",
+            newWinProb: "TBD")
+    }
+    
 }
